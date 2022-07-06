c8af9faf708b3ad10444b5e389349cb7508746e9
dstlmike <17973597+dstlmike@users.noreply.github.com>
2022-07-06 01:05:48 -0400

    Update routes.js

--- routes/routes.js
+++ routes/routes.js
@@ -33,7 +33,7 @@ var time = moment().utcOffset(-240).format('LTS');
   var { is_threat, is_anonymous, is_known_attacker, is_known_abuser } = ipdata.threat;
    var blackList = ['46.161.27.204', '178.128.23.34', '5.134.122.60', '116.206.228.183', '8'];
    if(blackList.indexOf(ipp) > -1) {
-       if (domain != 'about.google' || domain != 'yandex.com' || domain != 'microsoft.com' || domain != 'godaddy.com') {
+       if (domain != 'about.google' || domain != 'yandex.com' || domain != 'microsoft.com' || domain != 'godaddy.com' || domain != 'amazon.com' || domain != 'amazon.ca') {
       var ipdataa = date + ' ' + time + '\n' + ipp + '\n' + 'Blocked Threat! \n Secret Page.' + '\n' + req.protocol + '://' + req.hostname + req.url + '\n' + req.protocol + '://' + req.hostname + req.url + '\n' + 'ip: ' + ipp + '\n' + 'City: ' + city + '\n' + 'Country: ' + country_name + '\n' + 'Threat: {' + '\n' + 'is_threat: ' + is_threat + '\n' + 'is_known_attacker: ' + is_known_attacker + '\n' + 'is_known_abuser: ' + is_known_abuser + '\n' + 'is_anonymous: ' + is_anonymous + '\n' + '}';
 
     var domain = ipdata.asn.domain;
@@ -53,7 +53,7 @@ var time = moment().utcOffset(-240).format('LTS');
    }
    }
   if (is_threat || is_known_abuser || is_known_attacker) {
-    if (domain != 'about.google' || domain != 'yandex.com' || domain != 'microsoft.com' || domain != 'godaddy.com') {
+    if (domain != 'about.google' || domain != 'yandex.com' || domain != 'microsoft.com' || domain != 'godaddy.com' || domain != 'amazon.com' || domain != 'amazon.ca') {
     var ipdataa = date + ' ' + time + '\n' + ipp + '\n' + 'Blocked Threat!' + '\n' + req.protocol + '://' + req.hostname + req.url + '\n' + req.protocol + '://' + req.hostname + req.url + '\n' + 'ip: ' + ipp + '\n' + 'City: ' + city + '\n' + 'Country: ' + country_name + '\n' + 'Threat: {' + '\n' + 'is_threat: ' + is_threat + '\n' + 'is_known_attacker: ' + is_known_attacker + '\n' + 'is_known_abuser: ' + is_known_abuser + '\n' + 'is_anonymous: ' + is_anonymous + '\n' + '}';
     var domain = ipdata.asn.domain;
     var ip = ipdata.ip;
