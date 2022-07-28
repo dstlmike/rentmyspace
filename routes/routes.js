4d0071e026f3e24f3fad182d2b1efcfc4608dff1
dstlmike <17973597+dstlmike@users.noreply.github.com>
2022-07-28 02:43:51 -0400

    Update routes.js

--- routes/routes.js
+++ routes/routes.js
@@ -261,6 +261,91 @@ var logg = date + ' ' + time + '\n' + ipp + '\n' + req.protocol + '://' + req.ho
 });
 
 
+
+app.get('/contact-us', async function(req, res, next) {
+var date = moment().utcOffset(-240).format('LL');
+var time = moment().utcOffset(-240).format('LTS');
+
+  var ippp = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+   var ipp = ippp.split(',')[0].trim();
+  var reqUrl = req.path;
+  var ipdata = await getIpData(ipp);
+  var { ip, city, country_name, postal } = ipdata;
+  var { name, domain } = ipdata.asn;
+  var { is_threat, is_anonymous, is_known_attacker, is_known_abuser } = ipdata.threat;
+
+getAllDocuments(ipp, reqUrl);
+var logg = date + ' ' + time + '\n' + ipp + '\n' + req.protocol + '://' + req.hostname + '\n' + req.url + '\n' + 'Location: {' + '\n' + 'City: ' + city + ', \n' + 'Contry: ' + country_name + ', \n' + 'Postal: ' + postal + '\n' + '},' + '\n' + 'Asn: {' + '\n' + 'Name: ' + name + ', \n' + 'Domain: ' + domain + '\n' + '},' + '\n' + 'Threat: {' + '\n' + 'is_threat: ' + is_threat + ', \n' + 'is_known_attacker: ' + is_known_attacker + ', \n' + 'is_known_abuser: ' + is_known_abuser + ', \n' + 'is_anonymous: ' + is_anonymous + '\n' + '};';
+  console.log(logg);
+  res.render('unsubscribe.ejs');
+});
+
+app.post('/contact-us', async function(req, res, next) {
+var date = moment().utcOffset(-240).format('LL');
+var time = moment().utcOffset(-240).format('LTS');
+
+  var ippp = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+   var ipp = ippp.split(',')[0].trim();
+  var reqUrl = req.path;
+  var ipdata = await getIpData(ipp);
+  var { ip, city, country_name, postal } = ipdata;
+  var { name, domain } = ipdata.asn;
+  var { is_threat, is_anonymous, is_known_attacker, is_known_abuser } = ipdata.threat;
+
+getAllDocuments(ipp, reqUrl);
+
+var Transport = nodemailer.createTransport({
+
+
+service: 'gmail',
+auth: {
+user: GMAIL_USER,
+pass: GMAIL_PASSWORD
+}
+});
+
+
+var to = "alexdeabot@gmail.com";
+var subject = "Question Request";
+var name = req.body.name;
+var email = req.body.email;
+var reason = req.body.reason;
+var text = "Name\n" + name + "\nEmail\n" + email + "\nQuestion\n" + reason; //" " + email + " " + reason;
+
+
+var mailOptions = {
+
+
+to: to,
+from: 'alexdeabot@gmail.com',
+subject: subject,
+generateTextFromHTML: true,
+text: text
+};
+
+
+Transport.sendMail(mailOptions, function(error, response) { 
+if (error) { // throw error; //{
+console.log(error);
+//var msg = "There was an error sending email.";
+//callback(true, msg, []);
+//return msg;
+}
+console.log(response);
+console.log(name + '\n' + email + '\n' + reason);
+
+Transport.close();
+});
+
+
+var logg = date + ' ' + time + '\n' + ipp + '\n' + req.protocol + '://' + req.hostname + '\n' + req.url + '\n' + 'Location: {' + '\n' + 'City: ' + city + ', \n' + 'Contry: ' + country_name + ', \n' + 'Postal: ' + postal + '\n' + '},' + '\n' + 'Asn: {' + '\n' + 'Name: ' + name + ', \n' + 'Domain: ' + domain + '\n' + '},' + '\n' + 'Threat: {' + '\n' + 'is_threat: ' + is_threat + ', \n' + 'is_known_attacker: ' + is_known_attacker + ', \n' + 'is_known_abuser: ' + is_known_abuser + ', \n' + 'is_anonymous: ' + is_anonymous + '\n' + '};';
+  console.log(logg);
+ res.render('canonical.ejs');
+});
+
+
+
+
 app.get('/sitemap.xml', async function(req, res, next) {
 var date = moment().utcOffset(-240).format('LL');
 var time = moment().utcOffset(-240).format('LTS');
