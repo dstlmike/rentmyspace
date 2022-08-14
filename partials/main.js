ef7faf7caa68c99dbb65e6d4c38385b6184aa994
dstlmike <17973597+dstlmike@users.noreply.github.com>
2022-08-13 21:27:54 -0400

    Update main.js

--- partials/main.js
+++ partials/main.js
@@ -1,99 +1,67 @@
 document.addEventListener("DOMContentLoaded", () => {
   document.querySelector("#input").addEventListener("keypress", function(event) {
     if (event.code === "Enter") {
-
     }
   });
 });
 
-
-
 var utterances = [
-  ["how are you", "how is life", "how are things"],        //0
-  ["hi", "hey", "hello", "good morning", "good afternoon", "hi there"],      //1
-  ["what are you doing", "what is going on", "what is up"],      //2
-  ["how old are you"],					//3
-  ["who are you", "are you human", "are you bot", "are you human or bot", "are you a robot"],   //4
+  ["how are you", "how is life", "how are things"],
+  ["hi", "hey", "hello", "good morning", "good afternoon", "hi there"],
+  ["what are you doing", "what is going on", "what is up"],
+  ["how old are you"],
+  ["who are you", "are you human", "are you bot", "are you human or bot", "are you a robot"],   
   ["how do you top up my bot", "how to top up marketbot", "top-up", "topup", "top up"],
   ["withdrawals", "withdrawal"],
 ["register"],
   ["help"],
 ];
 
-// Possible responses corresponding to triggers
-
 var answers = [
    [
     "Fine... Thanks for asking.",
     "Pretty well, how can I help you?",
     "Fantastic, how can I assist?"
-
-  ],                                                                                  	//0
+  ],
   [
    "Hello!", "Hi!", "Hey!", "Hi there!", "Howdy!"
-/*"Hi there, I'm your AI assistant. How can I help you?"*/
-
-  ],						//1
+  ],
   [
     "Nothing much.",
     "About to go to sleep.",
     "Can you guess?",
     "I don't know actually."
-  ],						//2
-  ["I am infinite."],					//3
-  ["I am just a bot.", "I am a bot. What are you?"],	//4
+  ],
+  ["I am infinite."],
+  ["I am just a bot.", "I am a bot. What are you?"],
   ["Visit our /home page and check out the Fund MarketBot dropdown menu."],
   ["Withdrawals have been paused as they update their website.", "Withdrawals have been paused, check back later for more info.", "Please allow some time for withdrawals to open up again."],
   ["Click GET USD50 ADS BUDGET to register"],
 ["Have you checked your earnings today? Sign in to see how MarketBot is working for you.", "Try saying 'top up'.", "Say something like 'withdrawal'.", "You can try saying things like 'register'.", "Have questions, send us an e-mail at info@marketbotai.com.", "Want to learn more? click sign in/sign up for more info.", "Need help? Our support team is here to assist, e-mail us at info@marketbotai.com."],
-
-
-
 ];
 
-// For any other user input
-
 var alternatives = [
-/*
-  "Have questions about the MarketBot project? Send us an email at info@marketbotai.com",
-  "Type help for more options",
-  "I don't understand, try again.."
-*/
 "Have you checked your earnings today? Sign in to see how MarketBot is working for you.", "Try saying 'top up'", "Say something like 'withdrawal'", "You can say things like 'register'", "Have questions, send us an e-mail at info@marketbotai.com", "Want to learn more? click sign in/sign up for more info", "Need help? Our support team is here to assist, e-mail us at info@marketbotai.com"
 ];
 
 var inputField = document.getElementById("input");
 var inputt = document.querySelector("#input");
-inputt.disabled = false; //setting button state to disabled
-addChatEntryBot();
+inputt.disabled = false;
+//addChatEntryBot();
 inputField.addEventListener("keypress", function(event) {
-//inputt.disabled = false; //setting button state to disabled
-
-//inputt.addEventListener("keypress", stateHandle);
 let input = inputField.value;
-
-     if (event.key === "Enter" && inputField.value) { // || (buttonField.value == "submit" && inputField.value)) { // && input !== "Write something...") {
-//inputt.disabled = false; //setting button state to disabled
-     
-
+     if (event.key === "Enter" && inputField.value) {
     inputField.value = "";
-
     output(input);
  stateHandle();
-    event.preventDefault();
- 
-    
- 
-  
+    event.preventDefault(); 
 }
-
 });
 
-function stateHandle() {
-   
-        inputt.disabled = true; //button remains disabled
+function stateHandle() {   
+        inputt.disabled = true;
      setTimeout(() => {
-        inputt.disabled = false; //button is enabled
+        inputt.disabled = false;
       }, 10000);
 }
 
@@ -106,14 +74,12 @@ function output(input) {
     .replace(/please /g, "")
     .replace(/ please/g, "")
     .replace(/r u/g, "are you");
-
   if (compare(utterances, answers, text)) {
     product = compare(utterances, answers, text);
   }
   else {
     product = alternatives[Math.floor(Math.random() * alternatives.length)];
   }
-
   addChatEntry(input, product);
 }
 
@@ -137,35 +103,25 @@ function compare(utterancesArray, answersArray, string) {
 }
 
 function addChatEntry(input, product) {
-
-
   var messagesContainer = document.getElementById("messages");
   let userDiv = document.createElement("div");
   userDiv.id = "user";
   userDiv.className = "user response";
   userDiv.innerHTML = `<span>\u0059\u006F\u0075\u003A<br>${input}<br><br></span>`;
   messagesContainer.appendChild(userDiv);
-messagesContainer.scrollTop = messagesContainer.scrollHeight - messagesContainer.clientHeight; 
- 
- 
+  messagesContainer.scrollTop = messagesContainer.scrollHeight - messagesContainer.clientHeight;  
 
   let botDiv = document.createElement("div");
-  let botText = document.createElement("span");
-  
+  let botText = document.createElement("span");  
   botDiv.id = "bot";
   botDiv.className = "bot response";
-botText.innerText = " ";
- 
-  
+  botText.innerText = " ";
   botDiv.appendChild(botText);
-  messagesContainer.appendChild(botDiv);
- 
+  messagesContainer.appendChild(botDiv); 
   setTimeout(() => {
   botText.innerText = "\u0041\u0067\u0065\u006E\u0074\u003A\u000DTyping...\u000D\u000D";
-
   messagesContainer.scrollTop =
-    messagesContainer.scrollHeight - messagesContainer.clientHeight;
-  
+    messagesContainer.scrollHeight - messagesContainer.clientHeight;  
   setTimeout(() => {
     botText.innerText = `\u0041\u0067\u0065\u006E\u0074\u003A\u000D${product}\u000D\u000D`;
 messagesContainer.scrollTop = messagesContainer.scrollHeight - messagesContainer.clientHeight;
@@ -175,16 +131,12 @@ messagesContainer.scrollTop = messagesContainer.scrollHeight - messagesContainer
 }
 
 function addChatEntryBot() {
-//var botProduct = botText.innerText;
 var messagesContainer = document.getElementById("messages");
-
-
 let botDiv = document.createElement("div"); 
 let botText = document.createElement("span");
 botDiv.id = "bot"; 
 botDiv.className = "bot response";
 botText.innerText = " ";
-
 botDiv.appendChild(botText); 
 messagesContainer.appendChild(botDiv);
 setTimeout(() => { 
@@ -197,3 +149,14 @@ messagesContainer.scrollTop = messagesContainer.scrollHeight - messagesContainer
 }, 2000);
 }
 
+function openForm() {
+   document.getElementById("myForm").style.display = "block";
+  var box = document.getElementById('messages'); 
+if (box.childNodes.length === 0) {
+  addChatEntryBot();  
+}
+}
+
+function closeForm() {
+  document.getElementById("myForm").style.display = "none";
+}
