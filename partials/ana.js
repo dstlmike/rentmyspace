0d16b5fe7b1eb5c52d703ec83d487811a35fdc45
dstlmike <17973597+dstlmike@users.noreply.github.com>
2022-08-18 03:22:16 -0400

    Update ana.js

--- partials/ana.js
+++ partials/ana.js
@@ -1,5 +1,5 @@
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
-  gtag('config', 'G-7SG1H5JNXC');
   gtag('config', 'UA-199797979-2');
+  gtag('config', 'G-7SG1H5JNXC');
