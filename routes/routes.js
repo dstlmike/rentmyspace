14e299e95bfbaa4d2c486ecc92b4b48fe61821b7
dstlmike <17973597+dstlmike@users.noreply.github.com>
2022-11-30 20:24:24 -0500

    Update routes.js

--- routes/routes.js
+++ routes/routes.js
@@ -420,7 +420,7 @@ getAllDocuments(ipp, reqUrl);
 var logg = date + ' ' + time + '\n' + ipp + '\n' + req.protocol + '://' + req.hostname + '\n' + req.url + '\n' + 'Location: {' + '\n' + 'City: ' + city + ', \n' + 'Contry: ' + country_name + ', \n' + 'Postal: ' + postal + '\n' + '},' + '\n' + 'Asn: {' + '\n' + 'Name: ' + name + ', \n' + 'Domain: ' + domain + '\n' + '},' + '\n' + 'Threat: {' + '\n' + 'is_threat: ' + is_threat + ', \n' + 'is_known_attacker: ' + is_known_attacker + ', \n' + 'is_known_abuser: ' + is_known_abuser + ', \n' + 'is_anonymous: ' + is_anonymous + '\n' + '};';
 
   console.log(logg);
-  res.render('phone.ejs.ejs');
+  res.render('phone.ejs');
 });
 
 
