fbcf2bc02423a2407a6ced7745c01bc0bdd1e589
dstlmike <17973597+dstlmike@users.noreply.github.com>
2023-10-22 02:16:37 -0400

    Update main.js

--- partials/main.js
+++ partials/main.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
   document.querySelector("#input").addEventListener("keypress", function(event) {
-    if (event.code === "Enter") {
+   // if (event.code === "Enter") {
     }
   });
 });
